202a8151af41289503c109ec0723afcb78526995
Aditya10403 <adityashukla@gmail.com>
2024-05-29 12:27:26 +0530

    connect to mongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,23 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoDB = process.env.MONGO_DB;
+const port = process.env.PORT || 3000;
+
+mongoose
+  .connect(mongoDB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server listening on port 3000!");
-})
+app.listen(port, () => {
+  console.log("Server listening on port 3000!");
+});
